c781b4f6f4a54ca38243cef4cc09cb1899c773ba
ronedyglenn <ronedyglenn@gmail.com>
2023-12-15 20:22:05 +0100

    the static app

--- scripts/pop-up.js
+++ scripts/pop-up.js
@@ -1,11 +1,67 @@
-let popUp = document.querySelector('#pop-up')
-let button = document.querySelector(".btn-learner")
+/**
+ * Modifications liées au profile lorsqu'il s'agira d'ajouter un apprennant
+ */
 
-button.addEventListener("click", ()=> {
-    return declenchement();
+//Sélection de l'image de profile
+const profile = document.querySelector(".profile_apprennant");
+//récupération de la valeur de l'input *file
+const inputValue = document.querySelector("#btn-profile_apprennant");
+//Evennement qui permet de changer la photo de profile
+inputValue.addEventListener('change', ()=>{
+    profile.src= URL.createObjectURL(inputValue.files[0]);
 })
-function declenchement(){
-    popUp.classList.toggle('open')
-}
 
-const port = process.env.PORT || 3001
+/**
+ * Affichage du PopUp
+ */
+
+let ajouterApprenant = document.querySelector('.btn-learner')
+//selection du body
+let popUp = document.querySelector('.popUp_containerMove')
+//Sélection du boutton pour frmer le popUp
+let bouttonFermer = document.querySelector('#Annuler')
+//ajout de l'évènement sur le boutton ajouter un apprennant.
+ajouterApprenant.addEventListener('click', ()=>{
+    popUp.classList.toggle('popUp_container');
+
+})
+
+//Fermeture du popUp
+bouttonFermer.addEventListener('click', ()=>{
+    popUp.classList.remove('popUp_container');
+
+})
+
+/**
+ * Confirmation et suppréssion d'un apprenant.
+ */
+
+
+//Selection du boutton de suppression
+const drashButton = document.querySelector('.drash');
+drashButton.addEventListener('click', ()=>{
+    if(confirm('Voulez vraiment supprimer un apprenant?')){
+        alert("l'apprenant est supprimé");
+    }else{
+        console.log("Vous n'avez pas confirmé");
+    }
+})
+
+/**
+ * Il s'agit des modifications liées à l'apprenant
+ */
+
+let validerModif = document.querySelector('#valider-modif');
+let AnnulerModif = document.querySelector('#Annuler-modif');
+//Selection du boutton de modification
+let bouttonModify = document.querySelector(".modify")
+//Section du popUp de modification
+let modification = document.querySelector(".modifierProfileFinale")
+//Evennemnt
+bouttonModify.addEventListener('click', ()=>{
+    modification.classList.toggle("modifierProfile")
+})
+//Retirer la class pour faire disparaite le popUp
+.addEventListener('click', ()=>{
+    AnnulerModif.classList.remove("modifierProfile")
+})
