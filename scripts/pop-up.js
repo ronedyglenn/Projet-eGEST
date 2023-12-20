8a7f1a635909da6dc73ee17983b933c20838910f
ronedyglenn <ronedyglenn@gmail.com>
2023-12-20 09:35:09 +0100

    Opérations DELETE ET GET

--- scripts/pop-up.js
+++ scripts/pop-up.js
@@ -33,8 +33,6 @@ bouttonFermer.addEventListener('click', ()=>{
 
 })
 
-/**
- * Confirmation et suppréssion d'un apprenant.
- */
+
 
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,3 @@
-// fetch('http://localhost:3500/apprenant')
-// .then(res => res.json())
-// .then(data => console.log(data));
-
-
 /**
  * Il s'agit de la programation liée à l'ajout d'un apprenant dans le tableau;
  */
@@ -29,9 +24,7 @@ let filiere = document.querySelector("#filiere");
 let niveau = document.querySelector("#niveau");
 
 
-
 //Ajout de toutes les données dans la;
-
 async function poster_les_apprenanats(){
     await fetch('http://localhost:3500/apprenant', {
         method: 'POST',
@@ -89,50 +82,95 @@ async function poster_les_apprenanats(){
                     <td>${apprenant.filiere}</td>
                     <td>${apprenant.niveau}</td>
                     <td>
-                        <button class="modify"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                        <button class="modify ${apprenant.Id_apprenant}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                             <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                         </svg></button>
-                        <button class="drash"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                        <button class="drash ${apprenant.Id_apprenant}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                             <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
                         </svg></button>
                     </td>
                 </tr>
                 `)
-
+                
+                let modifyButton = document.querySelectorAll(".modify")
+                modifyButton.forEach(elem =>{
+                    elem.addEventListener('click', ()=>{
+                        
+                    })
+                })
                 //Selection du boutton de suppression
-                const drashButton = document.querySelector('.drash');
-                drashButton.addEventListener('click', ()=>{
-                    if(confirm('Voulez vraiment supprimer un apprenant?')){
-                        alert("l'apprenant est supprimé");
-                    }else{
-                        console.log("Vous n'avez pas confirmé");
-                    }
+                let drashButton = document.querySelectorAll(".drash");
+                drashButton.forEach(el=>{
+                    el.addEventListener('click', ()=>{
+                        console.log(el.classList[1]);
+                        if(confirm('Voulez-vous vraiment supprimer ?')){
+                            fetch('http://localhost:3500/apprenant/'+ el.classList[1], {
+                                method: 'DELETE',
+                            })
+                            .then(response => response.text())
+                            .then(data => console.log(data))
+                        }else{
+                            console.log("Vous n'avez pas supprimé !");
+                        }
+                    })
                 })
-
-                /**
-                 * Il s'agit des modifications liées à l'apprenant
-                 */
-
-                let validerModif = document.querySelector('#valider-modif');
-                let AnnulerModif = document.querySelector('#Annuler-modif');
-                //Selection du boutton de modification
-                let bouttonModify = document.querySelector(".modify")
-                //Section du popUp de modification
-                let modification = document.querySelector(".modifierProfileFinale")
-                //Evennemnt
-                bouttonModify.addEventListener('click', ()=>{
-                    modification.classList.toggle("modifierProfile")
-                })
-                //Retirer la class pour faire disparaite le popUp
-                bouttonModify.addEventListener('click', ()=>{
-                    AnnulerModif.classList.remove("modifierProfile")
-                })
-
+                
             })
         console.log(data)
 
                 
-        })
-    }
-
+    })
+}
     receiveData()
+
+    async function postData(){
+        //Selection du formulaire de connexion
+        const form = document.querySelector('form')
+        
+        /**
+         * Selection des différents champs
+        */
+       const nom = document.querySelector("#Nom1")
+       const prenom = document.querySelector("#Prenom1")
+       const num_tel = document.querySelector("#Number1")
+       const sexe = document.querySelector("#sexe1")
+       const email = document.querySelector("#email1")
+       const date_de_naissance = document.querySelector("#date1")
+       const filiere = document.querySelector("#filiere1")
+       const niveau = document.querySelector("#Niveau1")
+       
+       //les boutons du formulaire de modification
+       
+       const bouton1 = document.getElementById("valider-modif")
+       const bouton2 = document.getElementById("Annuler-Modif")
+       
+       //div Parent du formulaire
+       let parentForm = document.querySelector('.modifierProfileFinale')
+       parentForm.classList.toggle('modifierProfile')
+
+       bouton2.addEventListener('click', ()=>{
+        parentForm.classList.remove("modifierProfile")
+       })
+
+       let modification = document.classList[1]
+
+       //Récupération du bouton modify
+       bouton1.addEventListener('click', async ()=>{
+        return await fetch("http://localhost:3500/apprenant/", {
+            method: "PUT",
+            headers: "content-type : application/json",
+            body: JSON.stringify({
+            nom:  nom.value,
+            prenom: prenom.value,
+            sexe: sexe.value,
+            num_tel: num_tel.value,
+            email: email.value,
+            date_de_naissance: date_de_naissance.value,
+            filiere: filiere.value,
+            niveau: niveau.value,
+            })
+        })
+        .then(data => data.json())
+        .then(data => console.log(data))
+       })
+    }
